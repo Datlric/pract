645ae4789953eda7cd763770cc267b6cf8e127ed
zyp <2237126549@qq.com>
2022-02-20 18:13:34 +0800

    version 0.0.1 back-end

--- src/main/java/com/pract/service/impl/DeviceServiceImpl.java
+++ src/main/java/com/pract/service/impl/DeviceServiceImpl.java
@@ -14,11 +14,8 @@ import java.util.HashMap;
 @Service
 public class DeviceServiceImpl implements DeviceService {
 
-    private HashMap<String, ClientMqtt> deviceMap = RunWith.deviceMap;
-
     @Autowired
     private RedisUtils redisUtils;
-
 
     //初次访问对设备进行连接以及确认信息收到
     @Override
@@ -41,7 +38,7 @@ public class DeviceServiceImpl implements DeviceService {
             //如果client对应PushCallBack中的Message不为空的话，就将这个设备的信息放进deviceMap里面，RunWith类中会自动遍历
             if (!clientMqtt.getPushCallback().getMESSAGE().isEmpty()) {
                 //存储进全局工作设备信息的map，key: device_id; value: 客户端
-                deviceMap.put(deviceForConnect.getDevice_id(), clientMqtt);
+                RunWith.deviceMap.put(deviceForConnect.getDevice_id(), clientMqtt);
                 return deviceForConnect;
             } else {
                 //初始化接收到消息为空就关闭这个客户端
@@ -54,14 +51,22 @@ public class DeviceServiceImpl implements DeviceService {
 
 
     @Override
-    public String close(Device device) {
+    public Boolean close(Device device) {
+        boolean res = false;
         // todo 2.18 前端传参主动关闭设备待写
-        return null;
+        ClientMqtt clientMqtt = RunWith.deviceMap.get(device.getDevice_id());
+        if (clientMqtt != null) {
+            clientMqtt.close();
+            RunWith.deviceMap.remove(device.getDevice_id());
+            res = true;
+        } else RunWith.deviceMap.remove(device.getDevice_id());
+        return res;
     }
 
     @Override
     public String getCurrentMsg(Device device) {
         //todo 2.18 前端传参获取实时压力数据待写
-        return null;
+        HashMap<String, String> message = RunWith.deviceMap.get(device.getDevice_id()).getStandardMessage();
+        return JsonUtils.objectToJson(message);
     }
 }
